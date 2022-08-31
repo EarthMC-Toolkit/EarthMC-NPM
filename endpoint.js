5a2369cafcd3c741bba1a544c8a39c2f656c6d00
Owen <owen77stubbs@gmail.com>
2022-08-31 02:35:44 +0100

    Update endpoint.js

--- endpoint.js
+++ endpoint.js
@@ -1,23 +1,17 @@
 const fetch = require('node-fetch'),
       endpoints = () => fetch(`https://raw.githubusercontent.com/EarthMC-Toolkit/Toolkit-Website/main/endpoints.json`).then(res => res.json()),
-      get = (dataKey, worldKey) => endpoints().then(obj => obj[dataKey][worldKey].toString())
-
-const playerData = async mapName => {
-    let url = await get("players", mapName),
-        route = new URL(url)
-
-    return fetch(route).then(res => res.json()).catch(console.error)
-}
-
-const mapData = async mapName => {
-    let url = await get("map", mapName),
-        route = new URL(url)
-
-    return fetch(route).then(res => res.json()).catch(console.error)
-}
+      get = (dataType, world) => endpoints().then(obj => obj[dataType][world].toString()),
+      asJSON = url => fetch(new URL(url)).then(res => res.json()).catch(e => console.log(e))
 
 module.exports = {
-    get,
-    playerData,
-    mapData
+    get, asJSON,
+    getArchive: async (url, unixTs=Date.now()) => await asJSON(`https://web.archive.org/web/${unixTs.toString()}id_/${url}`),
+    playerData: async mapName => {
+        let url = await get("players", mapName)
+        return await asJSON(url)
+    },
+    mapData: async mapName => {
+        let url = await get("map", mapName)
+        return await asJSON(url)
+    }
 }
