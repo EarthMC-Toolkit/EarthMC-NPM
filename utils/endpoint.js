cb80d8c0498622c5c48b1ac89b0291b51c1019fd
Owen <owen77stubbs@gmail.com>
2022-09-24 20:56:23 +0100

    fix archive

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -2,7 +2,12 @@ const fetch = require('node-fetch'),
       endpoints = () => fetch(`https://raw.githubusercontent.com/EarthMC-Toolkit/Toolkit-Website/main/endpoints.json`).then(res => res.json()),
       get = (dataType, map) => endpoints().then(obj => obj[dataType][map].toString()),
       asJSON = url => fetch(new URL(url)).then(res => res.json()).catch(e => console.log(e)),
-      getArchive = async (url, unixTs=Date.now()) => await asJSON(`https://web.archive.org/web/${unixTs.toString()}id_/${decodeURIComponent(url)}`)
+      getArchive = async (url, unixTs=Date.now()) => {
+        let date = new Date(unixTs * 1000)
+        const formattedTs = date.toISOString().replace(/[^0-9]/g, '').slice(0, -3)
+
+        return await asJSON(`https://web.archive.org/web/${formattedTs}id_/${decodeURIComponent(url)}`)
+      }
 
 var archiveTs = false
 module.exports = {
