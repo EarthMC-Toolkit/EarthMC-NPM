a46e1a85ff97a80d9deca7f03a1ee5e0b2731ff7
Owen <owen77stubbs@gmail.com>
2024-06-24 16:17:32 +0100

    all my homies use tsc

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 import nodePolyfills from 'rollup-plugin-polyfill-node'
-import dts from 'rollup-plugin-dts'
+//import dts from 'rollup-plugin-dts'
 
 const generatedCode = {
     arrowFunctions: true,
@@ -47,13 +47,13 @@ const source = {
     ]
 }
 
-const types = {
-    input: 'src/types/index.ts',
-    output: [{ 
-        file: pkg.types, 
-        format: 'es'
-    }],
-    plugins: [dts()]
-}
+// const types = {
+//     input: 'src/types/index.ts',
+//     output: [{ 
+//         file: pkg.types, 
+//         format: 'es'
+//     }],
+//     plugins: [dts()]
+// }
 
-export default [source, types]
+export default [source]
