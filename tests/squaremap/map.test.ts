622106a0ce0e9ef4d827e197245296f1332b4cd4
Owen <owen77stubbs@gmail.com>
2025-01-02 02:07:01 +0000

    Update map.test.ts

--- tests/squaremap/map.test.ts
+++ tests/squaremap/map.test.ts
@@ -1,13 +1,11 @@
-import { describe, it, expect,  assertType } from 'vitest'
-
-import { Aurora } from '../../src/main'
+import { describe, it } from 'vitest'
 
 describe('[Squaremap/Aurora] Map', () => {
-    // Commented until `wealth` is added back to the popup.
-    // it('can get accurate total map wealth', async() => {
-    //     const wealth = await Aurora.Towns.totalWealth()
+    // Skipped until `wealth` is added back to the popup.
+    it.skip('can get accurate total map wealth', async() => {
+        // const wealth = await Aurora.Towns.totalWealth()
 
-    //     expect(wealth).toBeTruthy()
-    //     assertType<number>(wealth)
-    // })
+        // expect(wealth).toBeTruthy()
+        // assertType<number>(wealth)
+    })
 })
