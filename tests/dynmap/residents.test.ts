f80f940d947fcc3b03052b0061d200f52ae79c98
Owen <owen77stubbs@gmail.com>
2025-04-16 03:12:58 +0100

    .

--- tests/dynmap/residents.test.ts
+++ tests/dynmap/residents.test.ts
@@ -1,24 +1,24 @@
-import { describe, it, expect, expectTypeOf, assertType } from 'vitest'
-import { Resident } from '../../src/types'
+// import { describe, it, expect, expectTypeOf, assertType } from 'vitest'
+// import { Resident } from '../../src/types'
 
-import { Nova } from '../../src/main'
+// import { Nova } from '../../src/main'
 
-describe.skip('[Dynmap/Nova] Residents', () => {
-    it('can get all residents', async () => {
-        const residents = await Nova.Residents.all()
-        assertType<Resident[]>(residents)
-    })
+// describe.skip('[Dynmap/Nova] Residents', () => {
+//     it('can get all residents', async () => {
+//         const residents = await Nova.Residents.all()
+//         assertType<Resident[]>(residents)
+//     })
 
-    it('can get single resident', async () => {
-        const resident = await Nova.Residents.get('3meraldk') as Resident
+//     it('can get single resident', async () => {
+//         const resident = await Nova.Residents.get('3meraldk') as Resident
 
-        expect(resident).toBeDefined()
-        expectTypeOf(resident).not.toEqualTypeOf<Error>()
-        assertType<Resident | Resident[]>(resident)
+//         expect(resident).toBeDefined()
+//         expectTypeOf(resident).not.toEqualTypeOf<Error>()
+//         assertType<Resident | Resident[]>(resident)
 
-        expect(resident.name).toBe("3meraldK")
-        expect(resident.rank).toBe("Mayor")
-        //expect(resident.timestamps).toBeDefined()
-        //expect(resident.timestamps.registered).toEqual(1652454407381)
-    })
-})
+//         expect(resident.name).toBe("3meraldK")
+//         expect(resident.rank).toBe("Mayor")
+//         //expect(resident.timestamps).toBeDefined()
+//         //expect(resident.timestamps.registered).toEqual(1652454407381)
+//     })
+// })
--- tests/dynmap/towns.test.ts
+++ tests/dynmap/towns.test.ts
@@ -1,30 +1,30 @@
-import { 
-    describe, it, 
-    expect, assertType
-} from 'vitest'
+// import { 
+//     describe, it, 
+//     expect, assertType
+// } from 'vitest'
 
-import { Town } from '../../src/types'
-import { Nova } from '../../src/main'
+// import { Town } from '../../src/types'
+// import { Nova } from '../../src/main'
 
-describe.skip('[Dynmap/Nova] Towns', () => {
-    it('can get all towns', async () => {
-        const towns = await Nova.Towns.all()
-        assertType<Town[]>(towns)
-    })
+// describe.skip('[Dynmap/Nova] Towns', () => {
+//     it('can get all towns', async () => {
+//         const towns = await Nova.Towns.all()
+//         assertType<Town[]>(towns)
+//     })
 
-    it('can get single town', async () => {
-        const town = await Nova.Towns.get('kraftier') as Town
+//     it('can get single town', async () => {
+//         const town = await Nova.Towns.get('kraftier') as Town
 
-        expect(town).toBeTruthy()
-        expect(town).toBeDefined()
-        assertType<Town | Town[]>(town)
-    })
+//         expect(town).toBeTruthy()
+//         expect(town).toBeDefined()
+//         assertType<Town | Town[]>(town)
+//     })
 
-    it('can get towns invitable to specified nation', async () => {
-        const invitableTowns = await Nova.Towns.invitable('sudan')
+//     it('can get towns invitable to specified nation', async () => {
+//         const invitableTowns = await Nova.Towns.invitable('sudan')
 
-        expect(invitableTowns).toBeTruthy()
-        expect(invitableTowns).toBeDefined()
-        assertType<Town[]>(invitableTowns)
-    })
-})
+//         expect(invitableTowns).toBeTruthy()
+//         expect(invitableTowns).toBeDefined()
+//         assertType<Town[]>(invitableTowns)
+//     })
+// })
